94de327a708454bf83b62a21d5596fb155b0ca0c
Paul-Wisdom <wisdomp837@gmail.com>
2026-01-04 02:44:24 +0100

    reverting health check path to pre-failure state

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // simulating failed health check
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
